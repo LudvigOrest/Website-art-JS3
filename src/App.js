9a6b0ee25354c9a053ee3790bed3ab731a7af986
LudvigOrest <ludvig.orest@live.se>
2023-09-19 19:15:52 +0800

    Added Header component, improved header-styling in styles.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import './styles/styles.css'
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { Children } from 'react';
+
+function NavbarItem({ Body }) {
+    return(
+        <div class="navbar-items">{Body}</div>
+    );
+};
+
+
+function Header() {
+    return (
+        <container id="navbar">
+            <div id="navbar-sub">
+                <div class="navbar-sub-item" id="links">
+                    <NavbarItem Body="Konst" />
+                    <NavbarItem Body="Bilder" />
+                    <NavbarItem Body="Beställningar" />
+                    <NavbarItem Body="Kontakta" />
+                </div>
+                <div class="navbar-sub-item" id="header-item">
+                    <h1 id="header">OREST ART</h1>  
+                </div>
+                    <div class="navbar-sub-item">
+                    <div class="navbar-items" id="search-bar">Sök</div>
+                </div>
+                
+            </div>
+        </container>
+    );
+};
+
+export default Header;
